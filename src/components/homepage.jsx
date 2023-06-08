4885bc4a54c6f7b27da9d362d4b9a90e53cb06a2
Elias Montauti <egsm147@gmail.com>
2023-06-08 21:42:35 +0100

    75%

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from '../logos/icons/AEG.svg';
 import ovenPic from '../img/AEG_Showroom_28Fev2023_009A6079.JPG';
+import collageImg1 from '../img/009A3499.jpg';
+import collageImg2 from '../img/009A3512.jpg';
+import collageImg3 from '../img/AEG_Showroom_28Fev2023_009A6012.JPG';
+import collageImg4 from '../img/AEG_Showroom_28Fev2023_009A6079.JPG';
+import Img5 from '../img/009A3538.jpg';
+import Img6 from '../img/009A3546.jpg';
+
 
 export const HomePage = () => {
     return (
@@ -27,7 +34,6 @@ export const HomePage = () => {
             <section>
                 <div className='section-title'>
                     <h1>COZINHE A VAPOR * <br /> E PUPE 20% DE ENERGIA</h1>
-
                 </div>
                 <div className='saibaMais-content'>
                     <img src={ovenPic} alt="" className='saibaMais-img'/>
@@ -39,15 +45,27 @@ export const HomePage = () => {
                 </div>
                 <div className='grayLine'/>
             </section>
-            <section>
-                <div className='section-title'>
-                    <h1 style={{ color: 'red' }}>AEG HAUS</h1>
-                    <h1>A NOSSA NOVA CASA</h1>
+            <section className='novaCasa-container'>
+                <div className='novaCasa-subTitle'>
+                    <div className='section-title'>
+                        <h1 style={{ color: 'red' }}>AEG HAUS</h1>
+                        <h1>A NOSSA NOVA CASA</h1>
+                    </div>
+                    <p style={{color: 'white' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ab maxime recusandae expedita corporis nam illo itaque iusto omnis doloribus quisquam.</p>
                 </div>
-                <p style={{color: 'white' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ab maxime recusandae expedita corporis nam illo itaque iusto omnis doloribus quisquam.</p>
-                <div>
-                  
+                <div className='image-grid'>
+                    <img src={collageImg1} alt="img1" className='photoOne'/>
+                    <img src={collageImg2} alt="img2" className='photoTwo'/>
+                    <img src={collageImg3} alt="img3" className='photoThree'/>
+                    <img src={collageImg4} alt="img4" className='photoFour'/>
                 </div>
+                <p className='novaCasa-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fugiat aperiam deserunt. Deleniti eaque rem fuga repudiandae inventore porro sint dolore minima facere! Cum et quos commodi, sed facere velit? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum suscipit similique obcaecati impedit laboriosam repellat eaque illo ipsa ut commodi, exercitationem provident tenetur numquam eos illum sint iure deleniti tempore?</p>
+                <p className='novaCasa-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus culpa porro alias, harum labore excepturi a cupiditate inventore ipsa ratione commodi, modi rem ab debitis, odit quia soluta doloremque.</p>
+                <div className='image-box'>
+                    <img src={Img5} alt="img5" />
+                    <img src={Img6} alt="img6" />
+                </div>
+                <button className='novaCasa-btn'>{'Saiba mais      >'}</button>
             </section>
             <footer className='footer'>
                 <div className='footer-content1'>
